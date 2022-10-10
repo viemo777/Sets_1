9a533e8fb9c59211e3ee2c7532c5bb69d4f8e8cb
Vitalii Mocanu <viemo777@gmail.com>
2022-10-10 23:29:55 +0300

    commit 1

--- sets.py
+++ sets.py
@@ -1,6 +1,10 @@
 # Sets
+# unique elements
 # setname = {value1, value2, ...} # ex. x = {13, 45, 'WWW'}
 # empty_sets = set()
+# add() # ex. x.add(777) = {13, 45, 'WWW', 777}
+# remove() # ex. x.remove(45) = {13, 'WWW', 777}
+# discard() # ex. x.discard('WWW') = {13, '777}
 
 rainbow_colors = {'green', 'blue', 'white', 'black'}
 print(rainbow_colors)
@@ -8,3 +12,12 @@ print(rainbow_colors)
 empty_sets = set()
 print(empty_sets)
 print(type(empty_sets))
+
+rainbow_colors.add(777)
+print(rainbow_colors)
+
+#rainbow_colors.pop()
+#print(rainbow_colors)
+
+rainbow_colors.remove(777)
+print(rainbow_colors)
