379e3079464ec7a93af2fb0d3a0ecc0ec9ef8dee
Vitalii Mocanu <viemo777@gmail.com>
2022-10-10 22:32:41 +0300

    commit 1

---
+++ sets.py
@@ -0,0 +1,10 @@
+# Sets
+# setname = {value1, value2, ...} # ex. x = {13, 45, 'WWW'}
+# empty_sets = set()
+
+rainbow_colors = {'green', 'blue', 'white', 'black'}
+print(rainbow_colors)
+
+empty_sets = set()
+print(empty_sets)
+print(type(empty_sets))
